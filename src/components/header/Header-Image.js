a63370d64cc60f9b4a6b40db64f48baf25d465a0
arnav <schedulingvibesofficial@gmail.com>
2023-05-11 21:33:26 -0300

    Adding header files

---
+++ src/components/header/Header-Image.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+import styles from "Header-Image.module.scss";
+
+const HeaderImage = () => {
+  return <Fragment></Fragment>;
+};
+
+export default HeaderImage;
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+import HeaderImage from "./Header-Image";
+
+import styles from "Header.module.scss";
+
+const Header = () => {
+  return <></>;
+};
+
+export default Header;
