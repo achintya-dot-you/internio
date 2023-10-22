2674533f5324e816aae843f2d42fe79eac9e2485
arnav <arnav.bholausa@gmail.com>
2023-10-22 20:32:51 -0300

    refactoring code

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,8 +1,18 @@
 import React from "react";
-import styles from "./Mission.module.scss"; // Import your SCSS module
+
+import styles from "./Mission.module.scss";
 
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+
+import rawText from "../data/Mission.txt";
+
+let text = "";
+fetch(rawText)
+  .then((r) => r.text())
+  .then((t) => {
+    text = t;
+  });
 
 const Mission = () => {
   return (
@@ -10,10 +20,7 @@ const Mission = () => {
       <Navbar />
       <div className={styles["mission-container"]}>
         <h2 className={styles["mission-heading"]}>Our Mission</h2>
-        <p className={styles["mission-text"]}>
-          {/* TODO - ACHINTYA */}
-          At internio, our mission is to empower future leaders by providing motivated and driven high school students with quality internship opportunities. We believe in a future where high school students can seamlessly access internships, enabling them to put their skills into practice and gain invaluable work experience. Our goal is to remove the barriers that often hinder students from pursuing internships, ensuring that every young individual has the chance to excel academically and professionally. We aim to equip the next generation with the confidence, knowledge, and real-world skills they need to thrive in college and beyond. Together, we're building a brighter future, one internship at a time.
-        </p>
+        <p className={styles["mission-text"]}>{text}</p>
       </div>
       <Footer />
     </>
