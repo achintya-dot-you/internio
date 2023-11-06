e2df59d4c880765fb028cdf577093866e1bf883f
arnav <arnav.bholausa@gmail.com>
2023-11-05 23:53:00 -0400

    starting files for email

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import envCompatible from "vite-plugin-env-compatible";
-
-export default defineConfig({
-  envPrefix: "REACT_APP_",
-
-  plugins: [react(), envCompatible()],
-});
